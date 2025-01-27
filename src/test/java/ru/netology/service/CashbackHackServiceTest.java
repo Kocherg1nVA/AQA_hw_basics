76cf24b7937eb34f2f8416cb84673c4f1133df04
Vadim Kochergin <kochergin1990vadim@gmail.com>
2025-01-27 18:54:48 +0300

    junit4-platform test

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,8 +1,8 @@
 package ru.netology.service;
 
-import org.junit.Test;
-
 import static org.junit.Assert.*;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class CashbackHackServiceTest {
 
@@ -42,5 +42,40 @@ public class CashbackHackServiceTest {
         int expected = 0;
 
         assertEquals(expected, actual);
+    }
+    @Test
+    public void testRemainAPI() {
+
+        int actual = service.remain(900);
+        int expected = 100;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemain2API() {
+
+        int actual = service.remain(1500);
+        int expected = 500;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemain3API() {
+
+        int actual = service.remain(0);
+        int expected = 1000;
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void testRemain4API() {
+
+        int actual = service.remain(1000);
+        int expected = 0;
+
+        Assertions.assertEquals(expected, actual);
     }
 }
